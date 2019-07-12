b081d1b9e9eb96f70562c8327da3e44fb1249c80
nixswinner <nixondeveloper@gmail.com>
2019-07-12 23:13:43 +0300

    add vector drawables of info and person - use in the buttons

--- app/src/main/java/com/nix/alchallenge01/AboutALCActivity.java
+++ app/src/main/java/com/nix/alchallenge01/AboutALCActivity.java
@@ -2,17 +2,23 @@ package com.nix.alchallenge01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class AboutALCActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.setTitle("About ALC");
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://andela.com/alc/");
+
     }
 
 
