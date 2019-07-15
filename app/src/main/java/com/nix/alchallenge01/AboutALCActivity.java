ba260f5ef2c4bd3e1f17cd6338f13926f14536ca
nixswinner <nixondeveloper@gmail.com>
2019-07-15 08:05:40 +0300

    add my profile details

--- app/src/main/java/com/nix/alchallenge01/AboutALCActivity.java
+++ app/src/main/java/com/nix/alchallenge01/AboutALCActivity.java
@@ -2,13 +2,23 @@ package com.nix.alchallenge01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class AboutALCActivity extends AppCompatActivity {
     private WebView webView;
+    private WebViewClient webClient;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +27,30 @@ public class AboutALCActivity extends AppCompatActivity {
         this.setTitle("About ALC");
 
         webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webClient=new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                Toast.makeText(AboutALCActivity.this,"Error occured "+error.toString()+
+                        " loading about ALC Kindly try again.",Toast.LENGTH_SHORT).show();
+                Log.e("AboutALC error", "onReceivedError: "+error.toString() );
+                finish();
+            }
+        };
         webView.loadUrl("https://andela.com/alc/");
+        webView.setWebViewClient(webClient);
 
     }
 
